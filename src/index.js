62ea7c5bc4c33a80036ba031a4d63a3cef33efd8
Andrey Poznyak <andpoznyak@gmail.com>
2019-02-26 00:17:00 +0300

    added firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import 'normalize.css/normalize.css';
 import './styles/index.scss';
 
+import firebase from "firebase/app";
+import * as db from "firebase/database";
+
 document.addEventListener("DOMContentLoaded", () => {
-	console.log('here we go')
+	console.log('here we go');
+
+	const config = {
+		apiKey: "<API_KEY>",
+		authDomain: "<PROJECT_ID>.firebaseapp.com",
+		databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+		projectId: "<PROJECT_ID>",
+		//storageBucket: "<BUCKET>.appspot.com",
+		//messagingSenderId: "<SENDER_ID>",
+	};
+
+	firebase.initializeApp(config);
 });
