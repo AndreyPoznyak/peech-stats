c62b225317adf826106e1e7e0f5931c0576f10ab
Andrey Poznyak <andpoznyak@gmail.com>
2019-03-16 17:38:23 +0300

    added more details on retention

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './styles/index.scss';
 
 import {MDCRipple} from '@material/ripple';
 
+const apiBaseURL = 'https://us-central1-voiceservice-217021.cloudfunctions.net';
+
 let elements = {};
 
 const setupComponents = () => {
@@ -10,7 +12,9 @@ const setupComponents = () => {
         getDataButton: document.querySelector('.get-stats-data'),
         getUsersDataButton: document.querySelector('.get-users-data'),
         usersAmountLabel: document.querySelector('.users-amount'),
-        users21DaysLabel: document.querySelector('.users-retention-amount'),
+        users1DayLabel: document.querySelector('.users-retention-one-amount'),
+        users7DaysLabel: document.querySelector('.users-retention-seven-amount'),
+        users30DaysLabel: document.querySelector('.users-retention-thirty-amount'),
         usersActiveLabel: document.querySelector('.users-active-amount'),
         usersTenVoicedLabel: document.querySelector('.users-ten-voiced-amount'),
         usersTenArticlesLabel: document.querySelector('.users-ten-articles-amount'),
@@ -31,29 +35,36 @@ const notUsers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
 
 const filterNotRealUsers = users => users.filter(user => !notUsers.includes(user.userId || user.id));
 
-const fetchData = () => fetch('https://us-central1-voiceservice-217021.cloudfunctions.net/stats').then(response => response.json());
-const fetchUsers = () => fetch('https://us-central1-voiceservice-217021.cloudfunctions.net/users').then(response => response.json());
+const fetchData = () => fetch(`${apiBaseURL}/stats`).then(response => response.json());
+const fetchUsers = () => fetch(`${apiBaseURL}/users`).then(response => response.json());
 
 const getPercentage = (part, amount) => Math.round(100 * (part / amount));
 
 const populateStatsValues = allUsers => {
 	const users = filterNotRealUsers(allUsers);
 	const today = new Date();
-	const monthAgo = new Date().setDate(today.getDate() - 30);
-	const days21Ago = new Date().setDate(today.getDate() - 21);
+	const days30Ago = new Date().setDate(today.getDate() - 30);
+	const days7Ago = new Date().setDate(today.getDate() - 7);
+	const days1Ago = new Date().setDate(today.getDate() - 1);
 	const msPerDay = 1000 * 60 * 60 * 24;
 
-	const activeUsers = users.filter(user => new Date(user.lastArticleAddDate).getTime() > monthAgo);
-	const days21AgoUsers = users.filter(user => new Date(user.registrationDate).getTime() <= days21Ago);
+	const activeUsers = users.filter(user => new Date(user.lastArticleAddDate).getTime() > days30Ago);
+	const days1AgoUsers = users.filter(user => new Date(user.registrationDate).getTime() <= days1Ago);
+	const days7AgoUsers = users.filter(user => new Date(user.registrationDate).getTime() <= days7Ago);
+	const days30AgoUsers = users.filter(user => new Date(user.registrationDate).getTime() <= days30Ago);
 	const voicedUsers = users.filter(user => user.voicedArticlesCount > 0);
 	const voicedTenUsers = users.filter(user => user.voicedArticlesCount > 9);
 	const articlesTenUsers = users.filter(user => user.articlesCount > 9);
 	const artcilesAddedUsers = users.filter(user => user.articlesCount > 0);
-	const retention21Users = days21AgoUsers.filter(user => ( new Date(user.lastArticleAddDate).getTime() - new Date(user.registrationDate).getTime()) / msPerDay > 21);
+	const retention1Users = days1AgoUsers.filter(user => ( new Date(user.lastArticleAddDate).getTime() - new Date(user.registrationDate).getTime()) / msPerDay > 1);
+	const retention7Users = days7AgoUsers.filter(user => ( new Date(user.lastArticleAddDate).getTime() - new Date(user.registrationDate).getTime()) / msPerDay > 7);
+	const retention30Users = days30AgoUsers.filter(user => ( new Date(user.lastArticleAddDate).getTime() - new Date(user.registrationDate).getTime()) / msPerDay > 30);
 
 	elements.usersAmountLabel.innerHTML = users.length;
 	elements.usersActiveLabel.innerHTML = activeUsers.length;
-	elements.users21DaysLabel.innerHTML = `${retention21Users.length} / ${days21AgoUsers.length} = ${getPercentage(retention21Users.length, days21AgoUsers.length)}%`;
+	elements.users1DayLabel.innerHTML = `${retention1Users.length} / ${days1AgoUsers.length} = ${getPercentage(retention1Users.length, days1AgoUsers.length)}%`;
+	elements.users7DaysLabel.innerHTML = `${retention7Users.length} / ${days7AgoUsers.length} = ${getPercentage(retention7Users.length, days7AgoUsers.length)}%`;
+	elements.users30DaysLabel.innerHTML = `${retention30Users.length} / ${days30AgoUsers.length} = ${getPercentage(retention30Users.length, days30AgoUsers.length)}%`;
 	elements.usersTenVoicedLabel.innerHTML = voicedTenUsers.length;
 	elements.usersVoicedLabel.innerHTML = `${voicedUsers.length} / ${artcilesAddedUsers.length} = ${getPercentage(voicedUsers.length, artcilesAddedUsers.length)}% (${voicedUsers.length} / ${users.length} = ${getPercentage(voicedUsers.length, users.length)}%)`;
 	elements.usersAddedArticlesLabel.innerHTML = `${artcilesAddedUsers.length} / ${users.length} = ${getPercentage(artcilesAddedUsers.length, users.length)}%`;
